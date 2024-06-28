a9097b10cb6180f125fb39402bd499bd6da5aeba
tttmb <annop.email@gmail.com>
2024-06-28 09:56:31 +0700

    modified experience order

--- app/ui/component/experience.tsx
+++ app/ui/component/experience.tsx
@@ -43,6 +43,36 @@ const experienceList: ExperienceType[] = [
     ],
   },
   {
+    title: 'Lead Developer',
+    company: 'Maewnam IT Network Solution',
+    date: 'Jan 2022 - May 2022',
+    description: 'Developed software for company B',
+    reponsibilities: [
+      'Join meetings with users to understand and gather business requirements.',
+      'Plan with team members for estimating the number of man-days required for implementation.',
+      "Provide tasks to other team's members.",
+      'Discuss with team members the solutions to be used.',
+      "Manage and provide task for other team's members.",
+      'Implement features according to the planned schedule.',
+      'Review the code.',
+      'Continuously deploy updates.',
+    ],
+    technologies: [
+      'JavaScript',
+      'TypeScript',
+      'HTML',
+      'SCSS',
+      'Tailwind',
+      'Node.js',
+      'Express',
+      'Vue',
+      'MongoDB',
+      'Linux',
+      'Docker',
+      'Git',
+    ],
+  },
+  {
     title: 'Senior Programmer',
     company: 'MSC Sittipol',
     date: 'June 2021 - Aug 2022',
@@ -71,36 +101,6 @@ const experienceList: ExperienceType[] = [
       'Oracle WebLogic',
       'Git',
       'SVN',
-    ],
-  },
-  {
-    title: 'Lead Developer',
-    company: 'Maewnam IT Network Solution',
-    date: 'Jan 2022 - May 2022',
-    description: 'Developed software for company B',
-    reponsibilities: [
-      'Join meetings with users to understand and gather business requirements.',
-      'Plan with team members for estimating the number of man-days required for implementation.',
-      "Provide tasks to other team's members.",
-      'Discuss with team members the solutions to be used.',
-      "Manage and provide task for other team's members.",
-      'Implement features according to the planned schedule.',
-      'Review the code.',
-      'Continuously deploy updates.',
-    ],
-    technologies: [
-      'JavaScript',
-      'TypeScript',
-      'HTML',
-      'SCSS',
-      'Tailwind',
-      'Node.js',
-      'Express',
-      'Vue',
-      'MongoDB',
-      'Linux',
-      'Docker',
-      'Git',
     ],
   },
   {
@@ -143,7 +143,7 @@ export default function Experience() {
   return (
     <div className={`mb-4 mt-2 flex justify-center`}>
       <div className="flex max-w-[940px] flex-1 flex-col gap-4 font-inter">
-        <DownloadButton className='self-start'></DownloadButton>
+        <DownloadButton className="self-start"></DownloadButton>
         {experienceList.map((experienceItem, index) => (
           <Suspense
             fallback={<div>Loading.......</div>}
